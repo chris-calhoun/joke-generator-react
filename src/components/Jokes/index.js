d5ef0c4c57251e1e231ce275aff48ce293f91f08
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-07 11:02:59 -0600

    added switch statement to handle various states.

--- src/components/Jokes/index.js
+++ src/components/Jokes/index.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import getJoke from '../../helpers/data/jokeData';
 
 export default class Joke extends Component {
+  // created a property for the state of the joke
   state = {
-    joke: [],
-    isActive: false,
+    jokeState: 'none',
   };
 
+  // api call to get joke object on page load
   componentDidMount() {
     getJoke().then((resp) => {
       this.setState({
@@ -15,37 +16,73 @@ export default class Joke extends Component {
     });
   }
 
-  showElement = () => {
+  //  changes the state to setup
+  getFirstJoke = () => {
     this.setState({
-      isActive: true,
+      jokeState: 'setup',
     });
-    console.warn('show');
+  };
+
+  //  changes the state to punchline
+  getPunchline = () => {
+    this.setState({
+      jokeState: 'punchline',
+    });
+  };
+
+  // makes new api call and changes state to setup
+  getNewJoke = () => {
+    getJoke().then((resp) => {
+      this.setState({
+        joke: resp,
+        jokeState: 'setup',
+      });
+    });
   };
 
   render() {
-    const { joke } = this.state;
+    const { joke, jokeState } = this.state;
+    let jokeComponent;
 
+    switch (jokeState) {
+      case 'none':
+        jokeComponent = (
+          <div className='jokeContainer'>
+            <button className='btn btn-dark' onClick={this.getFirstJoke}>
+              Get a Joke
+            </button>
+          </div>
+        );
+        break;
+      case 'setup':
+        jokeComponent = (
+          <div className='jokeContainer'>
+            <div className='setup'>{joke.setup}</div>
+            <button className='btn btn-dark' onClick={this.getPunchline}>
+              Get Punchline
+            </button>
+          </div>
+        );
+        break;
+      case 'punchline':
+        jokeComponent = (
+          <div className='jokeContainer'>
+            <div className='punchline'>{joke.punchline}</div>
+            <button className='btn btn-dark' onClick={this.getNewJoke}>
+              Get New Joke
+            </button>
+          </div>
+        );
+        break;
+      default:
+        console.warn('Joke not found.');
+    }
     return (
       <div className='card'>
         <div className='card-header'>
           <img src='/REACT_JS.png' className='card-img-top' alt='...' />
         </div>
-        <div className='card-body'>
-        {this.state.isActive ? <button className='btn btn-dark'>
-                  Get Punchline
-                </button> : <button className='btn btn-dark' onClick={this.showElement}>
-            Get a Joke
-          </button>}
-          {this.state.isActive ? <p>Setup: {joke[2]}</p> : null}
-
-          {/* <button className='btn btn-dark'>
-                  Get Punchline
-                </button>
-          <p>Punchline: {joke[3]}</p>
-          <button className='btn btn-dark'>
-                  Get a New Joke
-                </button> */}
-        </div>
+        <div className='card-body'>{jokeComponent}</div>
       </div>
     );
   }
